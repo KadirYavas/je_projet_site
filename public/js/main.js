1489334b9fd1daa51c9248bb1b30de256b9a4816
Kadir <yavas00@hotmail.com>
2019-12-18 14:18:40 +0100

    debutjsmodal

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,47 @@
+// Color Switch
+
+let colorSwitch = document.querySelector('.colorSwitch')
+let btn1 = colorSwitch.querySelector('.btn1')
+let btn2 = colorSwitch.querySelector('.btn2')
+let bodyC = document.body
+let accueil = document.querySelector('.accueil')
+let h1 = accueil.querySelector('h1')
+let pAcc = accueil.querySelector('p')
+console.log(h1)
+
+btn2.addEventListener('click', () => {
+    bodyC.style.backgroundColor = "#242323"
+    btn1.style.backgroundColor = "white"
+    bodyC.style.color = "white"
+    h1.style.color = "black"
+    pAcc.style.color = "black"
+})
+btn1.addEventListener('click', () => {
+    bodyC.style.backgroundColor = "white"
+    bodyC.style.color = ""
+    h1.style.color = "#212529"
+    pAcc.style.color = "#212529"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Carousel
+
+
 let carousel = document.querySelector('.carousel')
 let buttons = carousel.querySelector('button')
 let divCarousel = document.querySelectorAll('.container')[2]
