609dfcfc5bf1d771193d87ac36b898bc46aec555
Kadir <yavas00@hotmail.com>
2019-12-18 10:19:36 +0100

    footerFin

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+let carousel = document.querySelector('.carousel')
+let buttons = carousel.querySelector('button')
+let divCarousel = document.querySelectorAll('.container')[2]
+let colDivCarouselUn = divCarousel.querySelectorAll('.col-3')[0]
+let colDivCarouselDeux = divCarousel.querySelectorAll('.col-3')[1]
+let colDivCarouselTrois = divCarousel.querySelectorAll('.col-3')[2]
+let colDivCarouselQuatre = divCarousel.querySelectorAll('.col-3')[3]
